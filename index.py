00f00e99ec4226390fd5e9960154d0bfcd279aae
Fatima Munir <81692316+fatima142munir@users.noreply.github.com>
2024-08-26 18:18:34 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+
+# factorial of a number 
+numF = int(input("Enter a number for Factorial: "))    
+factorial = 1
+for f in range(1,numF+1):
+    factorial = factorial* f
+print("Factorial of number",numF,"is",factorial)
+
+
